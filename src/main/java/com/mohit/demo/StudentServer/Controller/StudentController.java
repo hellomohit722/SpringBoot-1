480823f6d6ab7552fa2393d8df79e1d59be66396
Mohit <hllomohit722@gmail.com>
2026-07-17 14:49:05 +0530

    getting student information from database

--- src/main/java/com/mohit/demo/StudentServer/Controller/StudentController.java
+++ src/main/java/com/mohit/demo/StudentServer/Controller/StudentController.java
@@ -1,10 +1,10 @@
-package com.mohit.demo.StudentServer;
+package com.mohit.demo.StudentServer.Controller;
 
+import com.mohit.demo.StudentServer.Entity.Student;
+import com.mohit.demo.StudentServer.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
@@ -26,4 +26,24 @@ public class StudentController {
         }
         return  ResponseEntity.status(201).body(result);
     }
+
+    @GetMapping("/getStudent/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable int id){
+        Student student = studentService.getStudentById(id);
+        return ResponseEntity.status(200).body(student);
+    }
 }
+
+/**
+
+ */
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/mohit/demo/StudentServer/Service/StudentService.java
+++ src/main/java/com/mohit/demo/StudentServer/Service/StudentService.java
@@ -1,5 +1,7 @@
-package com.mohit.demo.StudentServer;
+package com.mohit.demo.StudentServer.Service;
 
+import com.mohit.demo.StudentServer.Entity.Student;
+import com.mohit.demo.StudentServer.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +28,9 @@ public class StudentService {
         studentRepository.save(student);
         return student;
 
+    }
+
+    public Student getStudentById(int id) {
+        return studentRepository.findById(id).orElse(null);
     }
 }
